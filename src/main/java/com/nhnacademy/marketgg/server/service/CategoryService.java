bd09ea8513b420c5d33eb958ab63ba3055bc5878
cororing <corock.dev@gmail.com>
2022-07-08 16:08:04 +0900

    Chore: Jenkins CI/CD 테스트 #54

--- src/main/java/com/nhnacademy/marketgg/server/service/CategoryService.java
+++
@@ -1,8 +0,0 @@
-package com.nhnacademy.marketgg.server.service;
-
-import com.nhnacademy.marketgg.server.dto.CategoryRegisterRequest;
-
-public interface CategoryService {
-
-    void createCategory(CategoryRegisterRequest categoryRequest);
-}
