ad741516d7236f4fa33d1b42bee6edeb65d083cc
Moomin <moominK228@gmail.com>
2022-07-08 15:28:14 +0900

    Test: 카테고리 전체 목록 조회 테스트 코드 작성 #3
    - Controller
    - Service

--- src/test/java/com/nhnacademy/marketgg/server/controller/CategoryControllerTest.java
+++ src/test/java/com/nhnacademy/marketgg/server/controller/CategoryControllerTest.java
@@ -1,8 +1,43 @@
 package com.nhnacademy.marketgg.server.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nhnacademy.marketgg.server.service.CategoryService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @WebMvcTest(CategoryController.class)
 class CategoryControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    CategoryService categoryService;
+
+    @Test
+    @DisplayName("카테고리 목록 조회 테스트")
+    void testRetrieveCategories() throws Exception {
+        when(categoryService.retrieveCategories()).thenReturn(List.of());
+
+        this.mockMvc.perform(get("/admin/v1/categories"))
+                    .andExpect(status().isOk());
+
+        verify(categoryService, times(1)).retrieveCategories();
+    }
 
 }
--- src/test/java/com/nhnacademy/marketgg/server/service/impl/DefaultCategoryServiceTest.java
+++ src/test/java/com/nhnacademy/marketgg/server/service/impl/DefaultCategoryServiceTest.java
@@ -1,10 +1,38 @@
 package com.nhnacademy.marketgg.server.service.impl;
 
+import com.nhnacademy.marketgg.server.dto.CategoryRetrieveResponse;
+import com.nhnacademy.marketgg.server.repository.CategoryRepository;
+import com.nhnacademy.marketgg.server.service.CategoryService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @Transactional
 class DefaultCategoryServiceTest {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @MockBean
+    private CategoryRepository categoryRepository;
+
+    @Test
+    @DisplayName("카테고리 목록 조회")
+    void testRetrieveCategories() {
+        when(categoryRepository.findAllCategories()).thenReturn(List.of(new CategoryRetrieveResponse()));
+
+        List<CategoryRetrieveResponse> categoryResponses = categoryService.retrieveCategories();
+
+        assertThat(categoryResponses).hasSize(1);
+    }
 
 }
