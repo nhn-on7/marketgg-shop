ba43efe6e446a555819ea5a07980fc781640d7c2
Moomin <moominK228@gmail.com>
2022-07-14 14:15:12 +0900

    Feat: 찜 조회 #74
    - pom.xml 에 QueryDsl 을 사용하기 위해 dependency 와 plugin 을 추가하였습니다.

--- src/main/java/com/nhnacademy/marketgg/server/controller/DibController.java
+++ src/main/java/com/nhnacademy/marketgg/server/controller/DibController.java
@@ -2,18 +2,22 @@ package com.nhnacademy.marketgg.server.controller;
 
 import com.nhnacademy.marketgg.server.dto.request.DibCreateRequest;
 import com.nhnacademy.marketgg.server.dto.request.DibDeleteRequest;
+import com.nhnacademy.marketgg.server.dto.response.DibRetrieveResponse;
 import com.nhnacademy.marketgg.server.service.DibService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/shop/v1/dibs")
@@ -30,6 +34,15 @@ public class DibController {
                              .location(URI.create("/shop/v1/dibs"))
                              .contentType(MediaType.APPLICATION_JSON)
                              .build();
+    }
+
+    @GetMapping
+    ResponseEntity<List<DibRetrieveResponse>> retrieveDibs(@PathVariable Long memberNo) {
+        List<DibRetrieveResponse> dibResponses = dibService.retrieveDibs(memberNo);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                             .location(URI.create("/shop/v1/dibs"))
+                             .body(dibResponses);
     }
 
     @DeleteMapping
---
+++ src/main/java/com/nhnacademy/marketgg/server/repository/dib/DibRepositoryCustom.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.marketgg.server.repository.dib;
+
+import com.nhnacademy.marketgg.server.dto.response.DibRetrieveResponse;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import java.util.List;
+
+@NoRepositoryBean
+public interface DibRepositoryCustom {
+
+    List<DibRetrieveResponse> findAllDibs(Long memberNo);
+
+}
