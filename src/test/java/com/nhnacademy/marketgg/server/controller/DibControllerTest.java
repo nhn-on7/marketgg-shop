5b6b8ec7bcd9b5760a6b901116a985c7171b545a
Moomin <moominK228@gmail.com>
2022-07-14 15:09:40 +0900

    Test: 찜 관리 테스트 공통 코드 작성 - controller 파트 #74

--- src/test/java/com/nhnacademy/marketgg/server/controller/DibControllerTest.java
+++ src/test/java/com/nhnacademy/marketgg/server/controller/DibControllerTest.java
@@ -1,4 +1,23 @@
 package com.nhnacademy.marketgg.server.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nhnacademy.marketgg.server.service.DibService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(DibController.class)
 public class DibControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    DibService dibService;
+
+
 }
