be72bc8d65912b07e09d4fdc63f3d31879bf5659
Moomin <moominK228@gmail.com>
2022-07-16 18:18:48 +0900

    Feat: Asset QueryDsl 적용 #91

---
+++ src/main/java/com/nhnacademy/marketgg/server/repository/asset/AssetRepositoryCustom.java
@@ -0,0 +1,7 @@
+package com.nhnacademy.marketgg.server.repository.asset;
+
+import org.springframework.data.repository.NoRepositoryBean;
+
+@NoRepositoryBean
+public interface AssetRepositoryCustom {
+}
