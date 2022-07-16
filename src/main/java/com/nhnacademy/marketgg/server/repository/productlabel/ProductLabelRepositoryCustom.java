de9c2e2782161b68e30c2234a82419b794907034
Moomin <moominK228@gmail.com>
2022-07-16 22:20:35 +0900

    Feat: ProductLabel QueryDsl 적용 #91

---
+++ src/main/java/com/nhnacademy/marketgg/server/repository/productlabel/ProductLabelRepositoryCustom.java
@@ -0,0 +1,7 @@
+package com.nhnacademy.marketgg.server.repository.productlabel;
+
+import org.springframework.data.repository.NoRepositoryBean;
+
+@NoRepositoryBean
+public interface ProductLabelRepositoryCustom {
+}
