1219cef79953687cc4a4ba07a1df8c5e3ff5285a
Moomin <moominK228@gmail.com>
2022-07-21 11:40:44 +0900

    Docs: 누락된 찜 JavaDoc 작성 #107

--- src/main/java/com/nhnacademy/marketgg/server/service/DibService.java
+++ src/main/java/com/nhnacademy/marketgg/server/service/DibService.java
@@ -11,22 +11,31 @@ import java.util.List;
  */
 public interface DibService {
 
-//here
+    /**
+     * 회원의 찜 목록에 찜을 등록합니다.
+     *
+     * @param memberId - 찜을 등록할 회원의 식별번호입니다.
+     * @param productId - 찜 목록에 등록될 상품의 식별번호입니다.
+     *
+     * @since 1.0.0
+     */
     void createDib(final Long memberId, final Long productId);
 
     /**
      * 회원의 찜 목록을 반환합니다.
      *
-     * @param memberId - 찜 목록을 조회할 회원의 고유 번호입니다.
+     * @param memberId - 찜 목록을 조회할 회원의 식별번호입니다.
      * @return 회원의 찜 목록을 List 로 반환합니다.
+     *
      * @since 1.0.0
      */
     List<DibRetrieveResponse> retrieveDibs(final Long memberId);
 
     /**
-     * 회원의 찜 하나를 삭제합니다.
+     * 회원의 찜 목록에서 찜을 삭제합니다.
      *
-     * @since 1.0.0
+     * @param memberId - 찜을 삭제할 회원의 식별번호입니다.
+     * @param productId - 찜 목록에서 삭제될 상품의 식별번호입니다.
      */
     void deleteDib(Long memberId, Long productId);
 
