efcafa4f3fb6c7d9a6a24ae143cda6fab844f247
Moomin <moominK228@gmail.com>
2022-07-21 17:05:39 +0900

    Docs: GlobalControllerAdvice JavaDoc 작성 #110

--- src/main/java/com/nhnacademy/marketgg/server/controller/GlobalControllerAdvice.java
+++ src/main/java/com/nhnacademy/marketgg/server/controller/GlobalControllerAdvice.java
@@ -28,6 +28,13 @@ public class GlobalControllerAdvice {
         return "Error: " + ex.getMessage();
     }
 
+    /**
+     * 유효성 검사 Exception 에 대해 관리하는 Handler 입니다.
+     *
+     * @param ex - 에러 정보를 담은 Exception 입니다.
+     * @return 해당 에러의 에러메세지를 반환합니다.
+     * @since 1.0.0
+     */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseBody
     private String handleNotValidException(final MethodArgumentNotValidException ex) {
