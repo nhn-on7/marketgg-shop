d2419833a7646650227974cd7b8bab7455c35082
coalong <minay0904@gmail.com>
2022-07-24 13:32:31 +0900

    Feat: 상품 문의 조회 Pagination  구현 #86
    - 상품 번호로 상품 문의 전체 조회 Pagination
    - 회원 번호로 상품 문의 전체 조회 Pagination

--- src/main/java/com/nhnacademy/marketgg/server/repository/productinquirypost/ProductInquiryPostRepositoryImpl.java
+++ src/main/java/com/nhnacademy/marketgg/server/repository/productinquirypost/ProductInquiryPostRepositoryImpl.java
@@ -4,46 +4,56 @@ import com.nhnacademy.marketgg.server.dto.response.ProductInquiryResponse;
 import com.nhnacademy.marketgg.server.entity.ProductInquiryPost;
 import com.nhnacademy.marketgg.server.entity.QProductInquiryPost;
 import com.querydsl.core.types.Projections;
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
+import java.util.List;
+
 public class ProductInquiryPostRepositoryImpl extends QuerydslRepositorySupport
-    implements ProductInquiryPostRepositoryCustom {
+        implements ProductInquiryPostRepositoryCustom {
 
     public ProductInquiryPostRepositoryImpl() {
         super(ProductInquiryPost.class);
     }
 
     @Override
-    public List<ProductInquiryResponse> findALLByProductNo(final Long id) {
+    public Page<ProductInquiryResponse> findALLByProductNo(final Long id, Pageable pageable) {
         QProductInquiryPost productInquiryPost = QProductInquiryPost.productInquiryPost;
 
-        return from(productInquiryPost)
-            .select(Projections.constructor(ProductInquiryResponse.class,
-                productInquiryPost.member,
-                productInquiryPost.title,
-                productInquiryPost.content,
-                productInquiryPost.isSecret,
-                productInquiryPost.adminReply,
-                productInquiryPost.createdAt))
-            .where(productInquiryPost.pk.productNo.eq(id))
-            .fetch();
+        List<ProductInquiryResponse> result = from(productInquiryPost)
+                .select(Projections.constructor(ProductInquiryResponse.class,
+                        productInquiryPost.member,
+                        productInquiryPost.title,
+                        productInquiryPost.content,
+                        productInquiryPost.isSecret,
+                        productInquiryPost.adminReply,
+                        productInquiryPost.createdAt))
+                .where(productInquiryPost.pk.productNo.eq(id))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        return new PageImpl<>(result, pageable, result.size());
     }
 
     @Override
-    public List<ProductInquiryResponse> findAllByMemberNo(final Long id) {
+    public Page<ProductInquiryResponse> findAllByMemberNo(final Long id, Pageable pageable) {
         QProductInquiryPost productInquiryPost = QProductInquiryPost.productInquiryPost;
 
-        return from(productInquiryPost)
-            .select(Projections.constructor(ProductInquiryResponse.class,
-                productInquiryPost.member,
-                productInquiryPost.title,
-                productInquiryPost.content,
-                productInquiryPost.isSecret,
-                productInquiryPost.adminReply,
-                productInquiryPost.createdAt))
-            .where(productInquiryPost.member.id.eq(id))
-            .fetch();
+        List<ProductInquiryResponse> result = from(productInquiryPost)
+                .select(Projections.constructor(ProductInquiryResponse.class,
+                        productInquiryPost.member,
+                        productInquiryPost.title,
+                        productInquiryPost.content,
+                        productInquiryPost.isSecret,
+                        productInquiryPost.adminReply,
+                        productInquiryPost.createdAt))
+                .where(productInquiryPost.member.id.eq(id))
+                .fetch();
+
+        return new PageImpl<>(result, pageable, result.size());
     }
 
 }
--- src/main/java/com/nhnacademy/marketgg/server/service/ProductInquiryPostService.java
+++ src/main/java/com/nhnacademy/marketgg/server/service/ProductInquiryPostService.java
@@ -2,6 +2,7 @@ package com.nhnacademy.marketgg.server.service;
 
 import com.nhnacademy.marketgg.server.dto.request.ProductInquiryRequest;
 import com.nhnacademy.marketgg.server.dto.response.ProductInquiryResponse;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -17,7 +18,7 @@ public interface ProductInquiryPostService {
      * 상품 문의 글에는 제목, 내용, 비밀 문의 글 여부가 있습니다.
      *
      * @param productInquiryRequest - 상품 문의 글을 생성하기 위한 DTO 입니다.
-     * @param id             - 상품 문의 글을 남길 상품의 PK 입니다.
+     * @param id                    - 상품 문의 글을 남길 상품의 PK 입니다.
      * @since 1.0.0
      */
     void createProductInquiry(ProductInquiryRequest productInquiryRequest, Long id);
@@ -25,25 +26,25 @@ public interface ProductInquiryPostService {
     /**
      * 상품에 대한 모든 상품 문의 글을 조회합니다.
      *
-     * @param id     - 조회할 상품의 PK 입니다.
-     * @return              - 상품 문의 글을 List 로 반환합니다.
+     * @param id - 조회할 상품의 PK 입니다.
+     * @return - 상품 문의 글을 List 로 반환합니다.
      */
-    List<ProductInquiryResponse> retrieveProductInquiryByProductId(Long id);
+    List<ProductInquiryResponse> retrieveProductInquiryByProductId(Long id, Pageable pageable);
 
     /**
      * 회원이 남긴 모든 상품 문의 글을 조회합니다.
      *
-     * @param id      - 조회할 회원의 PK 입니다.
-     * @return              - 상품 문의 글을 List 로 반환합니다.
+     * @param id - 조회할 회원의 PK 입니다.
+     * @return - 상품 문의 글을 List 로 반환합니다.
      */
-    List<ProductInquiryResponse> retrieveProductInquiryByMemberId(Long id);
+    List<ProductInquiryResponse> retrieveProductInquiryByMemberId(Long id, Pageable pageable);
 
     /**
      * 상품 문의 글에 대한 관리자의 답글을 상품 문의 글에 업데이트합니다.
      *
-     * @param inquiryReply      - 상품 문의 글에 대한 답글이 답긴 DTO 입니다.
-     * @param inquiryId         - 상품 문의 글의 PK 입니다.
-     * @param productId         - 상품의 PK 입니다.
+     * @param inquiryReply - 상품 문의 글에 대한 답글이 답긴 DTO 입니다.
+     * @param inquiryId    - 상품 문의 글의 PK 입니다.
+     * @param productId    - 상품의 PK 입니다.
      * @since 1.0.0
      */
     void updateProductInquiryReply(ProductInquiryRequest inquiryReply, Long inquiryId, Long productId);
@@ -51,8 +52,8 @@ public interface ProductInquiryPostService {
     /**
      * 상품 문의 글을 삭제합니다.
      *
-     * @param inquiryId     - 삭제할 상품 문의 글의 PK 입나다.
-     * @param productId     - 상품의 PK 입니다.
+     * @param inquiryId - 삭제할 상품 문의 글의 PK 입나다.
+     * @param productId - 상품의 PK 입니다.
      */
     void deleteProductInquiry(Long inquiryId, Long productId);
 }
