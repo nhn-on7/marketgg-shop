9eaebfa2116617ad3d9bea83821e08d5d54a1a9a
Moomin <moominK228@gmail.com>
2022-07-27 10:51:58 +0900

    Test: 1:1문의, 댓글 Service TC 작성 완료 #122

---
+++ src/test/java/com/nhnacademy/marketgg/server/service/impl/DefaultCustomerServiceCommentServiceTest.java
@@ -0,0 +1,91 @@
+package com.nhnacademy.marketgg.server.service.impl;
+
+import com.nhnacademy.marketgg.server.dto.request.MemberCreateRequest;
+import com.nhnacademy.marketgg.server.dto.response.CustomerServiceCommentDto;
+import com.nhnacademy.marketgg.server.entity.CustomerServiceComment;
+import com.nhnacademy.marketgg.server.entity.CustomerServicePost;
+import com.nhnacademy.marketgg.server.entity.Member;
+import com.nhnacademy.marketgg.server.mapper.impl.CustomerServiceCommentMapper;
+import com.nhnacademy.marketgg.server.repository.customerservicecomment.CustomerServiceCommentRepository;
+import com.nhnacademy.marketgg.server.repository.customerservicepost.CustomerServicePostRepository;
+import com.nhnacademy.marketgg.server.repository.member.MemberRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+@ExtendWith(MockitoExtension.class)
+@Transactional
+public class DefaultCustomerServiceCommentServiceTest {
+
+    @InjectMocks
+    DefaultCustomerServiceCommentService commentService;
+
+    @Mock
+    CustomerServiceCommentMapper commentMapper;
+
+    @Mock
+    CustomerServiceCommentRepository commentRepository;
+
+    @Mock
+    CustomerServicePostRepository postRepository;
+
+    @Mock
+    MemberRepository memberRepository;
+
+    private static CustomerServiceComment comment;
+    private static CustomerServicePost post;
+    private static Member member;
+
+    @BeforeAll
+    static void beforeAll() {
+        member = new Member(new MemberCreateRequest());
+        post = new CustomerServicePost(1L, member, null, "c", "t", "r", "s", LocalDateTime.now(), LocalDateTime.now());
+        comment = new CustomerServiceComment(1L, null, member, null, LocalDateTime.now());
+    }
+
+    @Test
+    @DisplayName("댓글 등록")
+    void testCreateComment() {
+        given(commentMapper.toEntity(any(CustomerServiceCommentDto.class))).willReturn(comment);
+        given(postRepository.findById(anyLong())).willReturn(Optional.of(post));
+        given(memberRepository.findById(anyLong())).willReturn(Optional.of(member));
+
+        commentService.createComment(1L, 1L, new CustomerServiceCommentDto());
+
+        then(commentRepository).should().save(any(CustomerServiceComment.class));
+    }
+
+    @Test
+    @DisplayName("댓글 단건 조회")
+    void testRetrieveComment() {
+        given(commentRepository.findById(anyLong())).willReturn(Optional.of(comment));
+
+        commentService.retrieveComment(1L);
+
+        then(commentRepository).should().findById(anyLong());
+    }
+
+    @Test
+    @DisplayName("게시글의 댓글 목록 조회")
+    void testRetrieveCommentsByInquiry() {
+        given(commentRepository.findByInquiry(anyLong())).willReturn(List.of());
+
+        commentService.retrieveCommentsByInquiry(1L);
+
+        then(commentRepository).should().findByInquiry(anyLong());
+    }
+}
--- src/test/java/com/nhnacademy/marketgg/server/service/impl/DefaultCustomerServicePostServiceTest.java
+++ src/test/java/com/nhnacademy/marketgg/server/service/impl/DefaultCustomerServicePostServiceTest.java
@@ -6,10 +6,12 @@ import com.nhnacademy.marketgg.server.dto.request.MemberCreateRequest;
 import com.nhnacademy.marketgg.server.dto.response.CustomerServicePostDto;
 import com.nhnacademy.marketgg.server.entity.Categorization;
 import com.nhnacademy.marketgg.server.entity.Category;
+import com.nhnacademy.marketgg.server.entity.CustomerServiceComment;
 import com.nhnacademy.marketgg.server.entity.CustomerServicePost;
 import com.nhnacademy.marketgg.server.entity.Member;
 import com.nhnacademy.marketgg.server.mapper.impl.CustomerServicePostMapper;
 import com.nhnacademy.marketgg.server.repository.category.CategoryRepository;
+import com.nhnacademy.marketgg.server.repository.customerservicecomment.CustomerServiceCommentRepository;
 import com.nhnacademy.marketgg.server.repository.customerservicepost.CustomerServicePostRepository;
 import com.nhnacademy.marketgg.server.repository.member.MemberRepository;
 import org.junit.jupiter.api.BeforeAll;
@@ -19,9 +21,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -29,16 +35,20 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
+import static org.mockito.BDDMockito.willDoNothing;
 
 @ExtendWith(MockitoExtension.class)
 @Transactional
 public class DefaultCustomerServicePostServiceTest {
 
     @InjectMocks
-    DefaultCustomerServicePostService customerServicePostService;
+    DefaultCustomerServicePostService postService;
 
     @Mock
-    CustomerServicePostRepository customerServicePostRepository;
+    CustomerServicePostRepository postRepository;
+
+    @Mock
+    CustomerServiceCommentRepository commentRepository;
 
     @Mock
     MemberRepository memberRepository;
@@ -47,30 +57,26 @@ public class DefaultCustomerServicePostServiceTest {
     CategoryRepository categoryRepository;
 
     @Mock
-    CustomerServicePostMapper mapper;
+    CustomerServicePostMapper postMapper;
 
-    private static CustomerServicePost customerServicePost;
+    private static CustomerServicePost post;
     private static Category category;
-    private static Categorization categorization;
     private static Member member;
-    private static CustomerServicePostDto customerServicePostDto;
-    private static MemberCreateRequest memberCreateRequest;
-    private static CategoryCreateRequest categoryCreateRequest;
-    private static CategorizationCreateRequest categorizationCreateRequest;
+    private static CustomerServicePostDto postDto;
 
     @BeforeAll
     static void beforeAll() {
-        customerServicePostDto = new CustomerServicePostDto();
-        memberCreateRequest = new MemberCreateRequest();
-        categoryCreateRequest = new CategoryCreateRequest();
-        categorizationCreateRequest = new CategorizationCreateRequest();
+        postDto = new CustomerServicePostDto();
+        MemberCreateRequest memberRequest = new MemberCreateRequest();
+        CategoryCreateRequest categoryRequest = new CategoryCreateRequest();
+        CategorizationCreateRequest categorizationRequest = new CategorizationCreateRequest();
 
-        member = new Member(memberCreateRequest);
-        categorization = new Categorization(categorizationCreateRequest);
-        category = new Category(categoryCreateRequest, categorization);
-        customerServicePost = new CustomerServicePost(1L, member, category,
-                                                      null, null, null, null,
-                                                      LocalDateTime.now(), LocalDateTime.now());
+        member = new Member(memberRequest);
+        Categorization categorization = new Categorization(categorizationRequest);
+        category = new Category(categoryRequest, categorization);
+        post = new CustomerServicePost(1L, member, category,
+                                       null, null, null, null,
+                                       LocalDateTime.now(), LocalDateTime.now());
     }
 
     @Test
@@ -79,27 +85,79 @@ public class DefaultCustomerServicePostServiceTest {
         given(memberRepository.findById(anyLong())).willReturn(Optional.of(member));
         given(categoryRepository.findById(anyString())).willReturn(Optional.of(category));
         given(categoryRepository.retrieveCategoryIdByName(anyString())).willReturn(Optional.of("702"));
-        given(mapper.toEntity(any(CustomerServicePostDto.class))).willReturn(customerServicePost);
+        given(postMapper.toEntity(any(CustomerServicePostDto.class))).willReturn(post);
 
-        customerServicePostService.createOtoInquiry(1L, customerServicePostDto);
+        postService.createOtoInquiry(1L, postDto);
 
-        then(customerServicePostRepository).should().save(any(CustomerServicePost.class));
+        then(postRepository).should().save(any(CustomerServicePost.class));
     }
 
     @Test
     @DisplayName("고객센터 게시글 단건 조회")
     void testRetrieveCustomerServicePost() {
-        given(customerServicePostRepository.findById(anyLong())).willReturn(Optional.ofNullable(customerServicePost));
-        given(mapper.toDto(any(CustomerServicePost.class))).willReturn(customerServicePostDto);
+        given(postRepository.findById(anyLong())).willReturn(Optional.of(post));
+        given(postMapper.toDto(any(CustomerServicePost.class))).willReturn(postDto);
 
-        customerServicePostService.retrieveCustomerServicePost(1L);
+        postService.retrieveCustomerServicePost(1L);
 
-        then(customerServicePostRepository).should().findById(anyLong());
+        then(postRepository).should().findById(anyLong());
     }
 
     @Test
     @DisplayName("1:1 문의 목록 조회")
     void testRetrieveOtoInquiries() {
-        
+        given(categoryRepository.retrieveCategoryIdByName(anyString())).willReturn(Optional.of("702"));
+        given(postRepository.findPostByCategoryId(any(Pageable.class), anyString()))
+                .willReturn(Page.empty());
+
+        postService.retrieveOtoInquiries(PageRequest.of(0, 10));
+
+        then(postRepository).should().findPostByCategoryId(any(Pageable.class), anyString());
+    }
+
+    @Test
+    @DisplayName("특정 회원의 1:1 문의 목록 조회")
+    void testRetrieveOwnOtoInquiries() {
+        given(categoryRepository.retrieveCategoryIdByName(anyString())).willReturn(Optional.of("702"));
+        given(postRepository.findPostByCategoryAndMember(any(Pageable.class), anyString(), anyLong()))
+                .willReturn(Page.empty());
+
+        postService.retrieveOwnOtoInquiries(PageRequest.of(0, 10), 1L);
+
+        then(postRepository).should()
+                            .findPostByCategoryAndMember(any(Pageable.class), anyString(), anyLong());
+    }
+
+    @Test
+    @DisplayName("고객센터 게시글 삭제(댓글이 없을 경우)")
+    void testDeleteCustomerServicePostNotExistsComments() {
+        given(postRepository.findById(anyLong())).willReturn(Optional.ofNullable(post));
+        given(commentRepository.findByInquiry(anyLong())).willReturn(List.of());
+
+        willDoNothing().given(postRepository).delete(any(CustomerServicePost.class));
+
+        postService.deleteCustomerServicePost(1L);
+
+        then(postRepository).should().findById(anyLong());
+        then(postRepository).should().delete(any(CustomerServicePost.class));
+        then(commentRepository).should().findByInquiry(anyLong());
+    }
+
+    @Test
+    @DisplayName("고객센터 게시글 삭제(댓글이 있을 경우)")
+    void testDeleteCustomerServicePostExistsComments() {
+        CustomerServiceComment customerServiceComment = new CustomerServiceComment(1L, post, member,
+                                                                                   "content", LocalDateTime.now());
+        given(postRepository.findById(anyLong())).willReturn(Optional.of(post));
+        given(commentRepository.findByInquiry(anyLong())).willReturn(List.of(customerServiceComment));
+
+        willDoNothing().given(postRepository).delete(any(CustomerServicePost.class));
+        willDoNothing().given(commentRepository).deleteAll(List.of(customerServiceComment));
+
+        postService.deleteCustomerServicePost(1L);
+
+        then(postRepository).should().findById(anyLong());
+        then(postRepository).should().delete(any(CustomerServicePost.class));
+        then(commentRepository).should().findByInquiry(anyLong());
     }
 }
