067c1df731ccfb856d8a0e6fce014bec6f1dd2de
com-sun <gnlslfflffl@gmail.com>
2022-07-28 11:49:07 +0900

    Test: 후기 전체 조회 테스트코드 작성 #148

--- src/test/java/com/nhnacademy/marketgg/server/controller/ReviewControllerTest.java
+++ src/test/java/com/nhnacademy/marketgg/server/controller/ReviewControllerTest.java
@@ -2,21 +2,26 @@ package com.nhnacademy.marketgg.server.controller;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.BDDMockito.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nhnacademy.marketgg.server.dto.request.DefaultPageRequest;
 import com.nhnacademy.marketgg.server.dto.request.ReviewCreateRequest;
+import com.nhnacademy.marketgg.server.dto.response.common.SingleResponse;
 import com.nhnacademy.marketgg.server.service.ReviewService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -50,5 +55,16 @@ class ReviewControllerTest {
             .andExpect(status().isCreated());
 
         then(reviewService).should().createReview(any(ReviewCreateRequest.class), anyString());
+    }
+
+    @Test
+    @DisplayName("후기 전체 조회 테스트")
+    void testRetrieveReviews() throws Exception {
+        given(reviewService.retrieveReviews(new DefaultPageRequest().getPageable())).willReturn(new SingleResponse<>());
+
+        this.mockMvc.perform(get("/products/{productId}/review/", 1L))
+            .andExpect(status().isOk());
+
+        then(reviewService).should().retrieveReviews(any(Pageable.class));
     }
 }
--- src/test/java/com/nhnacademy/marketgg/server/service/impl/DefaultReviewServiceTest.java
+++ src/test/java/com/nhnacademy/marketgg/server/service/impl/DefaultReviewServiceTest.java
@@ -1,29 +1,35 @@
 package com.nhnacademy.marketgg.server.service.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.BDDMockito.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 
 import com.nhnacademy.marketgg.server.dto.request.MemberCreateRequest;
 import com.nhnacademy.marketgg.server.dto.request.ReviewCreateRequest;
+import com.nhnacademy.marketgg.server.dto.response.ReviewResponse;
+import com.nhnacademy.marketgg.server.dto.response.common.SingleResponse;
 import com.nhnacademy.marketgg.server.entity.Asset;
 import com.nhnacademy.marketgg.server.entity.Member;
 import com.nhnacademy.marketgg.server.entity.Review;
 import com.nhnacademy.marketgg.server.repository.asset.AssetRepository;
 import com.nhnacademy.marketgg.server.repository.member.MemberRepository;
 import com.nhnacademy.marketgg.server.repository.review.ReviewRepository;
-import com.nhnacademy.marketgg.server.service.ReviewService;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -47,6 +53,7 @@ class DefaultReviewServiceTest {
     private Review review;
     private Asset asset;
     private ReviewCreateRequest reviewRequest;
+    private ReviewResponse reviewResponse;
 
     @BeforeEach
     void setUp() {
@@ -60,6 +67,16 @@ class DefaultReviewServiceTest {
         ReflectionTestUtils.setField(reviewRequest, "content", "리뷰내용");
         ReflectionTestUtils.setField(reviewRequest, "rating", 5L);
         review = new Review(reviewRequest, member, asset);
+
+        reviewResponse = new ReviewResponse(1L,
+                                            1L,
+                                            1L,
+                                            "content",
+                                            5L,
+                                            true,
+                                            LocalDateTime.now(),
+                                            LocalDateTime.now(),
+                                            null);
     }
 
     @Test
@@ -72,6 +89,19 @@ class DefaultReviewServiceTest {
         this.reviewService.createReview(reviewRequest, "admin");
 
         then(reviewRepository).should().save(any(Review.class));
+    }
+
+    @Test
+    @DisplayName("후기 전체 조회 테스트")
+    void testRetrieveReviews() {
+        List<ReviewResponse> list = List.of(reviewResponse);
+        Page page = new PageImpl<>(list, PageRequest.of(0, 1), 1);
+        given(reviewRepository.retrieveReviews(PageRequest.of(0, 1))).willReturn(page);
+
+        SingleResponse<Page> reviewResponses = reviewService.retrieveReviews(page.getPageable());
+
+        assertThat(reviewResponses).isNotNull();
+        then(reviewRepository).should().retrieveReviews(page.getPageable());
     }
 
 }
