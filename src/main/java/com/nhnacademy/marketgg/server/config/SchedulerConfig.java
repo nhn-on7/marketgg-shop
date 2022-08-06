359c1ce994c3297855437ff98f8f79f144eceeed
coalong <minay0904@gmail.com>
2022-08-07 04:32:22 +0900

    Feat: 스케줄러 다중 스레드 사용하기 위한 Thread Pool Size 설정

---
+++ src/main/java/com/nhnacademy/marketgg/server/config/SchedulerConfig.java
@@ -0,0 +1,20 @@
+package com.nhnacademy.marketgg.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+@Configuration
+public class SchedulerConfig implements SchedulingConfigurer {
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+
+        threadPoolTaskScheduler.setPoolSize(5);
+        threadPoolTaskScheduler.initialize();
+
+        taskRegistrar.setTaskScheduler(threadPoolTaskScheduler);
+    }
+}
