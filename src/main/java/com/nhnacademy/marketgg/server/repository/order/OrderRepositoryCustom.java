619c8457866adbf1d395c7bd19d8f3015903853f
coalong <minay0904@gmail.com>
2022-08-08 01:18:55 +0900

    Feat: Order Repository 기능 추가
    - 회원 정보로 전 월 결제 완료 건에 대한 조회 메소드 추가

--- src/main/java/com/nhnacademy/marketgg/server/repository/order/OrderRepositoryCustom.java
+++ src/main/java/com/nhnacademy/marketgg/server/repository/order/OrderRepositoryCustom.java
@@ -1,7 +1,13 @@
 package com.nhnacademy.marketgg.server.repository.order;
 
+import com.nhnacademy.marketgg.server.entity.Member;
+import com.nhnacademy.marketgg.server.entity.Order;
+import java.util.List;
 import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
 public interface OrderRepositoryCustom {
+
+    List<Order> findOrderByMonth(Member member)  ;
+
 }
--- src/main/java/com/nhnacademy/marketgg/server/repository/order/OrderRepositoryImpl.java
+++ src/main/java/com/nhnacademy/marketgg/server/repository/order/OrderRepositoryImpl.java
@@ -1,6 +1,14 @@
 package com.nhnacademy.marketgg.server.repository.order;
 
+import static com.nhnacademy.marketgg.server.constant.OrderStatus.PAY_COMPLETE;
+
+import com.nhnacademy.marketgg.server.entity.Member;
 import com.nhnacademy.marketgg.server.entity.Order;
+import com.nhnacademy.marketgg.server.entity.QOrder;
+import com.querydsl.core.types.Projections;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
 public class OrderRepositoryImpl extends QuerydslRepositorySupport implements OrderRepositoryCustom {
@@ -9,4 +17,22 @@ public class OrderRepositoryImpl extends QuerydslRepositorySupport implements Or
         super(Order.class);
     }
 
+    @Override
+    public List<Order> findOrderByMonth(Member member) {
+        QOrder order = QOrder.order;
+
+        LocalDate previousMonth = LocalDate.now().minusMonths(1L);
+        LocalDateTime firstDate = previousMonth.withDayOfMonth(1).atStartOfDay();
+        LocalDateTime lastDate = previousMonth.withDayOfMonth(previousMonth.lengthOfMonth()).atStartOfDay();
+
+        return from(order)
+            .select(Projections.constructor(Order.class,
+                order.id, order.member, order.totalAmount, order.orderStatus, order.usedPoint, order.trackingNo,
+                order.createdAt, order.updatedAt, order.deletedAt))
+            .where(order.member.id.eq(member.getId())
+                                  .and(order.orderStatus.eq(PAY_COMPLETE.status()))
+                                  .and(order.createdAt.between(firstDate, lastDate)))
+            .fetch();
+
+    }
 }
