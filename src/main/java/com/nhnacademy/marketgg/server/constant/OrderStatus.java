f9b32ab22f7635bb0afae8960569f7da6b1cd6d1
coalong <minay0904@gmail.com>
2022-08-08 01:19:20 +0900

    Feat: Order 주문 상태 Enum 추가

---
+++ src/main/java/com/nhnacademy/marketgg/server/constant/OrderStatus.java
@@ -0,0 +1,20 @@
+package com.nhnacademy.marketgg.server.constant;
+
+public enum OrderStatus {
+
+    PAY_WAITING("결제대기"),
+    PAY_COMPLETE("결제완료"),
+    CANCEL_WAITING("취소/환불접수"),
+    CANCEL_COMPLETE("취소/환불완료");
+
+    private final String status;
+
+    OrderStatus(String status) {
+        this.status = status;
+    }
+
+    public String status() {
+        return this.status;
+    }
+
+}
