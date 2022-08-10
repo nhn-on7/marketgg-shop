ef9bbc4c1b158abdce6454130642d60de19f7dae
Moomin <moominK228@gmail.com>
2022-08-10 18:29:58 +0900

    Feat: 주문폼 출력목록 조회 완료 #178

---
+++ src/main/java/com/nhnacademy/marketgg/server/dto/response/deliveryaddress/DeliveryAddressResponse.java
@@ -0,0 +1,20 @@
+package com.nhnacademy.marketgg.server.dto.response.deliveryaddress;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class DeliveryAddressResponse {
+
+    private final Long id;
+
+    private final Boolean isDefaultAddress;
+
+    private final Integer zipCode;
+
+    private final String address;
+
+    private final String detailAddress;
+
+}
--- src/main/java/com/nhnacademy/marketgg/server/repository/deliveryaddress/DeliveryAddressRepositoryImpl.java
+++ src/main/java/com/nhnacademy/marketgg/server/repository/deliveryaddress/DeliveryAddressRepositoryImpl.java
@@ -1,12 +1,32 @@
 package com.nhnacademy.marketgg.server.repository.deliveryaddress;
 
+import com.nhnacademy.marketgg.server.dto.response.deliveryaddress.DeliveryAddressResponse;
 import com.nhnacademy.marketgg.server.entity.DeliveryAddress;
+import com.nhnacademy.marketgg.server.entity.QDeliveryAddress;
+import com.querydsl.core.types.Projections;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.util.List;
 
 public class DeliveryAddressRepositoryImpl extends QuerydslRepositorySupport implements DeliveryAddressRepositoryCustom {
 
     public DeliveryAddressRepositoryImpl() {
         super(DeliveryAddress.class);
+    }
+
+    @Override
+    public List<DeliveryAddressResponse> findDeliveryAddressesByMemberId(final Long memberId) {
+        QDeliveryAddress deliveryAddress = QDeliveryAddress.deliveryAddress;
+
+        return from(deliveryAddress)
+                .where(deliveryAddress.member.id.eq(memberId))
+                .select(Projections.constructor(DeliveryAddressResponse.class,
+                                                deliveryAddress.id,
+                                                deliveryAddress.isDefaultAddress,
+                                                deliveryAddress.zipCode,
+                                                deliveryAddress.address,
+                                                deliveryAddress.detailAddress))
+                .fetch();
     }
 
 }
