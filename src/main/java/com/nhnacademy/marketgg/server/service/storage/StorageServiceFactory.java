7fde6f059a13c60e2f09fea14c23c0f569e135be
com-sun <gnlslfflffl@gmail.com>
2022-08-10 19:26:13 +0900

    Feat: Spring Configuration 설정

--- src/main/java/com/nhnacademy/marketgg/server/service/storage/StorageServiceFactory.java
+++ src/main/java/com/nhnacademy/marketgg/server/service/storage/StorageServiceFactory.java
@@ -6,11 +6,22 @@ import java.util.Map;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
+/**
+ * StorageService 빈 주입을 동적으로 하기 위한 팩토리입니다.
+ * 상황에 따라 LocalStorageService와 NhnStorageService를 주입합니다.
+ *
+ * @author 조현진
+ */
 @Component
 public class StorageServiceFactory {
 
     private final Map<String, StorageService> storagesServices = new HashMap<>();
 
+    /**
+     * 스프링 컨테이너가 초기화 될 때 빈을 주입받습니다.
+     *
+     * @param storageServices - LocalStorageService, NhnStorageService
+     */
     public StorageServiceFactory(List<StorageService> storageServices) {
         if (CollectionUtils.isEmpty(storageServices)) {
             throw new IllegalArgumentException("주입할 StorageService가 없습니다.");
