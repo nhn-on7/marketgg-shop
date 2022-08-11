2291ddc16e9ee652bec6cc6822cbe9d1adb887d6
com-sun <gnlslfflffl@gmail.com>
2022-08-11 21:16:07 +0900

    Test: 파일 컨트롤러 테스트 추가

--- src/main/java/com/nhnacademy/marketgg/server/controller/file/FileController.java
+++ src/main/java/com/nhnacademy/marketgg/server/controller/file/FileController.java
@@ -16,7 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
+ * 파일을 관리하기 위한 controller입니다.
  *
+ * @author 조현진
  */
 @RestController
 @RequiredArgsConstructor
@@ -26,6 +28,12 @@ public class FileController {
 
     private final FileService fileService;
 
+    /**
+     * 이미지를 조회합니다.
+     *
+     * @param assetId - 자원번호입니다.
+     * @return 이미지 응답객체를 반환합니다.
+     */
     @GetMapping("/{assetId}")
     public ResponseEntity<ImageResponse> retrieveImage(@PathVariable final Long assetId) {
 
@@ -34,9 +42,19 @@ public class FileController {
         return new ResponseEntity<>(imageResponse, HttpStatus.OK);
     }
 
+    /**
+     * Toast UI를 사용해서 사진 등록을 하는 경우 이 메소드가 실행됩니다.
+     * 사진 등록을 누르는 순간 해당 사진이 Storage로 업로드 된 후, 해당 사진의 메타 데이터를 담은 응답객체를 반환합니다.
+     * 응답객체엔 사진의 url, 이름이 담겨있습니다.
+     *
+     * @param image - 이미지 파일입니다.
+     * @return - 이미지의 메타데이터를 담은 응답객체를 반환합니다.
+     * @throws IOException - IOException을 던집니다.
+     */
     // @RoleCheck
     @PostMapping
-    public ResponseEntity<ImageResponse> uploadAndRetrieveImage(@RequestBody final MultipartFile image) throws IOException {
+    public ResponseEntity<ImageResponse> uploadAndRetrieveImage(@RequestBody final MultipartFile image)
+        throws IOException {
 
         ImageResponse imageResponse = fileService.uploadImage(image);
 
