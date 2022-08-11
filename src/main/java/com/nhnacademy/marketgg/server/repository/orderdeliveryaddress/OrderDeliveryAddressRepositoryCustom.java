7650c0b8fc364db69cfb2173991b6879c671ba87
Moomin <moominK228@gmail.com>
2022-08-11 14:29:09 +0900

    Fix: 주문 조회시 회원은 삭제된 내역을 보지 못하도록 수정 #178

--- src/main/java/com/nhnacademy/marketgg/server/repository/orderdeliveryaddress/OrderDeliveryAddressRepositoryCustom.java
+++ src/main/java/com/nhnacademy/marketgg/server/repository/orderdeliveryaddress/OrderDeliveryAddressRepositoryCustom.java
@@ -1,7 +1,10 @@
 package com.nhnacademy.marketgg.server.repository.orderdeliveryaddress;
 
+import com.nhnacademy.marketgg.server.dto.response.orderdeliveryaddress.OrderDeliveryAddressResponse;
 import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
 public interface OrderDeliveryAddressRepositoryCustom {
+    OrderDeliveryAddressResponse findByOrderId(Long orderId);
+
 }
