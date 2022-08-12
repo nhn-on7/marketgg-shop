f656141ad2affb0aeb63b860072315ce7a6b41ab
Moomin <moominK228@gmail.com>
2022-08-12 11:38:43 +0900

    Test: Controller TC 셋업 #178

--- src/test/java/com/nhnacademy/marketgg/server/controller/OrderControllerTest.java
+++ src/test/java/com/nhnacademy/marketgg/server/controller/OrderControllerTest.java
@@ -1,13 +1,39 @@
 package com.nhnacademy.marketgg.server.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nhnacademy.marketgg.server.annotation.Role;
 import com.nhnacademy.marketgg.server.aop.AuthInjectAspect;
 import com.nhnacademy.marketgg.server.aop.MemberInfoAspect;
 import com.nhnacademy.marketgg.server.aop.RoleCheckAspect;
 import com.nhnacademy.marketgg.server.aop.UuidAspect;
+import com.nhnacademy.marketgg.server.dto.info.MemberInfo;
+import com.nhnacademy.marketgg.server.dummy.Dummy;
+import com.nhnacademy.marketgg.server.entity.Cart;
+import com.nhnacademy.marketgg.server.repository.member.MemberRepository;
+import com.nhnacademy.marketgg.server.service.order.OrderService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.Collections;
+import java.util.Optional;
+import java.util.UUID;
+
+import static com.nhnacademy.marketgg.server.aop.AspectUtils.AUTH_ID;
+import static com.nhnacademy.marketgg.server.aop.AspectUtils.WWW_AUTHENTICATE;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @Transactional
 @SpringBootTest
@@ -20,6 +46,44 @@ import org.springframework.transaction.annotation.Transactional;
 })
 public class OrderControllerTest {
 
+    MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper mapper;
+
+    @MockBean
+    OrderService orderService;
+
+    @MockBean
+    MemberRepository memberRepository;
+
+    String baseUri = "/orders";
+
+    MemberInfo member;
+    HttpHeaders headers;
+    String uuid;
+    Long memberId = 1L;
+
+    @BeforeEach
+    void setUp(WebApplicationContext wac) throws JsonProcessingException {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac)
+                                 .alwaysDo(print())
+                                 .build();
+
+        uuid = UUID.randomUUID().toString();
+        MemberInfo memberInfo = Dummy.getDummyMemberInfo(memberId, new Cart());
+        given(memberRepository.findMemberInfoByUuid(uuid)).willReturn(Optional.of(memberInfo));
+
+        String roles = mapper.writeValueAsString(Collections.singletonList(Role.ROLE_USER));
+        headers = new HttpHeaders();
+        headers.set(AUTH_ID, uuid);
+        headers.set(WWW_AUTHENTICATE, roles);
+    }
+
+    @Test
+    @DisplayName("주문 등록")
+    public void testCreateOrder() {
+
+    }
 
 }
