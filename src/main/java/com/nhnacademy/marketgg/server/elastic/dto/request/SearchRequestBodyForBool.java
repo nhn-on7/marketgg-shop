51fafd5103059d3e9c1c91f29f8a86c112934e34
psw4887 <psw4887@naver.com>
2022-08-13 17:12:33 +0900

    Fix: 검색 요청 객체 하나로 통일 #214

--- src/main/java/com/nhnacademy/marketgg/server/elastic/dto/request/SearchRequestBodyForBool.java
+++ src/main/java/com/nhnacademy/marketgg/server/elastic/dto/request/SearchRequestBodyForBool.java
@@ -62,6 +62,7 @@ public class SearchRequestBodyForBool<T> {
      *
      * @param sortMap    - 결과 목록의 정렬기준입니다.
      * @param request    - 검색을 진행할 검색정보를 담은 객체입니다.
+     * @param convertString - 검색 시 한/영 오타 교정 단어입니다.
      * @param optionCode - 검색을 진행할 옵션 값입니다.
      * @param option     - 검색을 진행할 옵션입니다.
      * @since 1.0.0
@@ -80,7 +81,7 @@ public class SearchRequestBodyForBool<T> {
         this.query = new BoolQuery(
                 new Bool(new Must(List.of(new MultiMatch(optionCode, NO_FUZZINESS, CATEGORY_FIELD),
                                           new MultiMatch(request.getRequest(), FUZZINESS, requestOption),
-                                          new MultiMatch(convertString, FUZZINESS, requestOption)))));
+                                          new MultiMatch(convertString, NO_FUZZINESS, requestOption)))));
     }
 
     /**
@@ -104,7 +105,24 @@ public class SearchRequestBodyForBool<T> {
                 new Bool(new Must(List.of(new MultiMatch(categoryCode, NO_FUZZINESS, CATEGORY_FIELD),
                                  new MultiMatch(optionCode, NO_FUZZINESS, List.of(option)),
                                  new MultiMatch(request.getRequest(), FUZZINESS, DEFAULT_BOARD_FIELD),
-                                 new MultiMatch(convertString, FUZZINESS, DEFAULT_BOARD_FIELD)))));
+                                 new MultiMatch(convertString, NO_FUZZINESS, DEFAULT_BOARD_FIELD)))));
+    }
+
+    /**
+     * 조건없이 검색할 시 검색 요청 객체를 생성해줍니다.
+     *
+     * @param sortMap - 검색의 정렬 기준입니다.
+     * @param request - 검색을 진행 할 정보를 담은 객체입니다.
+     * @param convertString - 검색 시 한/영 오타 교정 단어입니다.
+     * @since 1.0.0
+     */
+    public SearchRequestBodyForBool(final T sortMap, final SearchRequest request, final String convertString) {
+        this.sort = Collections.singletonList(sortMap);
+        this.from = request.getPage();
+        this.size = request.getSize();
+        this.query = new BoolQuery(
+                new Bool(new Must(List.of(new MultiMatch(request.getRequest(), FUZZINESS, DEFAULT_PRODUCT_FIELD),
+                                          new MultiMatch(convertString, NO_FUZZINESS, DEFAULT_BOARD_FIELD)))));
     }
 
     private Boolean isBoard(final String document) {
