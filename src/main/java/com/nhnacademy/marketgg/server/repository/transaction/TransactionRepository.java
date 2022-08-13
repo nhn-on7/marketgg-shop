9ab08a011b80f78f2e5a6794626a1154cdb19ec6
cororing <corock.dev@gmail.com>
2022-08-13 22:59:17 +0900

    Feat: 거래 테이블 관련 엔티티 및 저장소 삭제 #217

--- src/main/java/com/nhnacademy/marketgg/server/repository/transaction/TransactionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nhnacademy.marketgg.server.repository.transaction;
-
-import com.nhnacademy.marketgg.server.entity.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionRepository extends JpaRepository<Transaction, Transaction.Pk>, TransactionRepositoryCustom {
-
-}
