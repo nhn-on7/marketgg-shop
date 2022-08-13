80fb0f58b927b4bb70ca827186c2f76e53f2f973
cororing <corock.dev@gmail.com>
2022-08-13 23:00:32 +0900

    Feat: 결제 관련 엔티티 추가 #217

---
+++ src/main/java/com/nhnacademy/marketgg/server/entity/payment/CardPayment.java
@@ -0,0 +1,110 @@
+package com.nhnacademy.marketgg.server.entity.payment;
+
+import com.nhnacademy.marketgg.server.constant.payment.AcquireStatus;
+import com.nhnacademy.marketgg.server.constant.payment.AgencyCode;
+import com.nhnacademy.marketgg.server.constant.payment.CardType;
+import com.nhnacademy.marketgg.server.constant.payment.OwnerType;
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 카드 결제 개체입니다.
+ *
+ * @author 김정민
+ * @author 김훈민
+ * @author 민아영
+ * @author 박세완
+ * @author 윤동열
+ * @author 이제훈
+ * @author 조현진
+ * @version 1.0
+ * @since 1.0
+ */
+@Table(name = "card_payments")
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class CardPayment {
+
+    /**
+     * 결제 테이블의 키를 정의합니다.
+     *
+     * @author 이제훈
+     */
+    @Embeddable
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @Getter
+    @EqualsAndHashCode
+    public static class Pk implements Serializable {
+
+        @Column(name = "payment_no")
+        @NotNull
+        private Long paymentId;
+
+    }
+
+    @Id
+    private Pk pk;
+
+    @MapsId(value = "paymentId")
+    @OneToOne
+    @JoinColumn(name = "payment_no")
+    @NotNull
+    private Payment payment;
+
+    @Column
+    @NotNull
+    private Long amount;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    @NotBlank
+    @Size(min = 2, max = 10)
+    private AgencyCode companyCode;
+
+    @Column
+    @NotBlank
+    @Size(min = 2, max = 10)
+    private String number;
+
+    @Column
+    private Integer installmentPlanMonths;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    @NotBlank
+    @Size(min = 2, max = 10)
+    private CardType cardType;
+
+    @Column
+    @NotBlank
+    @Size(min = 2, max = 10)
+    private OwnerType ownerType;
+
+    @Column
+    @NotBlank
+    @Size(min = 2, max = 255)
+    private String receiptUrl;
+
+    @Column
+    @NotBlank
+    @Size(min = 2, max = 20)
+    private AcquireStatus acquireStatus;
+
+}
---
+++ src/main/java/com/nhnacademy/marketgg/server/entity/payment/Payment.java
@@ -0,0 +1,100 @@
+package com.nhnacademy.marketgg.server.entity.payment;
+
+import com.nhnacademy.marketgg.server.constant.payment.PaymentMethod;
+import com.nhnacademy.marketgg.server.constant.payment.PaymentStatus;
+import com.nhnacademy.marketgg.server.entity.Order;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 결제 개체입니다.
+ *
+ * @author 김정민
+ * @author 김훈민
+ * @author 민아영
+ * @author 박세완
+ * @author 윤동열
+ * @author 이제훈
+ * @author 조현진
+ * @version 1.0
+ * @since 1.0
+ */
+@Table(name = "payments")
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "payment_no")
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "order_no")
+    @NotNull
+    private Order order;
+
+    @Column(name = "order_name")
+    @NotBlank
+    @Size(max = 100)
+    private String orderName;
+
+    @Column(name = "payment_key", unique = true)
+    @NotBlank
+    @Size(max = 200)
+    private String paymentKey;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    @NotBlank
+    @Size(max = 20)
+    private PaymentMethod method;
+
+    @Column(name = "total_amount")
+    @NotNull
+    private Long totalAmount;
+
+    @Column(name = "balance_amount")
+    private Long balanceAmount;
+
+    @Column(name = "discount_amount")
+    private Long discountAmount;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus status;
+
+    @Column
+    @NotBlank
+    @Size(max = 64)
+    private String transactionKey;
+
+    @Column(name = "created_at")
+    @NotNull
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @NotNull
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    @NotNull
+    private LocalDateTime deletedAt;
+
+}
