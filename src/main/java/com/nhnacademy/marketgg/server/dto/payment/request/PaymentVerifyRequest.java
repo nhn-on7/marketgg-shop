2fc645f4543d7d67b20600e0aed33fbc0f8dde10
cororing <corock.dev@gmail.com>
2022-08-16 17:11:07 +0900

    Feat: 결제 검증 요청 객체 추가 #227
    (OrderToPayment 와 동기화 필요)

--- src/main/java/com/nhnacademy/marketgg/server/dto/payment/request/PaymentVerifyRequest.java
+++ src/main/java/com/nhnacademy/marketgg/server/dto/payment/request/PaymentVerifyRequest.java
@@ -1,4 +1,26 @@
 package com.nhnacademy.marketgg.server.dto.payment.request;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
 public class PaymentVerifyRequest {
+
+    private final String orderId; // prefix + orderId
+
+    private final String orderName;
+
+    private final String memberName;
+
+    private final String memberEmail;
+
+    private final Long totalAmount;
+
+    private final Long couponId;
+
+    private final Integer usedPoint;
+
+    private final Integer expectedSavePoint;
+
 }
