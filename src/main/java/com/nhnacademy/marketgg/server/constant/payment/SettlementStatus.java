b6c9bb08d956d0ca78478a14da2c6b5f6b0b9fdf
cororing <corock.dev@gmail.com>
2022-08-16 14:16:00 +0900

    Fix: 결제대행사 버전 업그레이드에 의해 응답 데이터 값 변경으로 인한 열거형 수정 #223

--- src/main/java/com/nhnacademy/marketgg/server/constant/payment/SettlementStatus.java
+++ src/main/java/com/nhnacademy/marketgg/server/constant/payment/SettlementStatus.java
@@ -14,8 +14,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum SettlementStatus {
 
-    INCOMPLETE("정산 미완료"),
-    COMPLETE("정산 완료");
+    INCOMPLETED("정산 미완료"),
+    COMPLETED("정산 완료");
 
     private final String name;
 
