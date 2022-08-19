91d3942dc9c0d00393416e76490969d176f25198
Moomin <moominK228@gmail.com>
2022-08-19 16:29:57 +0900

    Fix: 공통 응답 객체 변경된 부분 수정 #229

--- src/main/java/com/nhnacademy/marketgg/server/dto/ErrorEntity.java
+++ src/main/java/com/nhnacademy/marketgg/server/dto/ErrorEntity.java
@@ -4,17 +4,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 /**
- * Market GG 에서 제공하는 오류에 대한 클래스입니다.
- *
- * @author 김정민
- * @author 김훈민
- * @author 민아영
- * @author 박세완
- * @author 윤동열
- * @author 이제훈
- * @author 조현진
- * @version 1.0
- * @since 1.0
+ * Market GG 에서 제공하는 오류에 대한 정보를 담고 있는 클래스입니다.
  */
 @RequiredArgsConstructor
 @Getter
