703f56e5926d193e9152c73294d41a06ee9d1a66
bunsung92 <bunsung92@naver.com>
2022-08-21 01:40:06 +0900

    Style: 불필요한 Import 제거 #243

--- src/main/java/com/nhnacademy/marketgg/server/controller/delivery/DeliveryController.java
+++ src/main/java/com/nhnacademy/marketgg/server/controller/delivery/DeliveryController.java
@@ -6,8 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
