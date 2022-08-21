116fe7c73cc193bd99c410c2eb51e3508a840145
cororing <corock.dev@gmail.com>
2022-08-21 15:07:01 +0900

    Feat: 결제 수단에 따른 응답 결과 데이터셋 설정 #239

---
+++ src/main/java/com/nhnacademy/marketgg/server/dto/payment/result/Receipt.java
@@ -0,0 +1,12 @@
+package com.nhnacademy.marketgg.server.dto.payment.result;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class Receipt {
+
+    private String url;
+
+}
