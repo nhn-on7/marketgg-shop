977667a7eb9b5f9c5049bd158901ca69f0759122
cororing <corock.dev@gmail.com>
2022-08-21 14:46:53 +0900

    Fix: 결제 관련 요청 객체 모호성 제거 및 문서화 #239

--- src/main/java/com/nhnacademy/marketgg/server/dto/response/order/OrderToPayment.java
+++ src/main/java/com/nhnacademy/marketgg/server/dto/response/order/OrderToPayment.java
@@ -1,7 +1,6 @@
 package com.nhnacademy.marketgg.server.dto.response.order;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
