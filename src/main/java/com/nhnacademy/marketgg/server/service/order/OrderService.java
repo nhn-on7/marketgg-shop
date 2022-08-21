cc0e2757a3d619f4e4ffc0ae5440dca04814f17b
com-sun <gnlslfflffl@gmail.com>
2022-08-21 21:55:40 +0900

    Fix: typo

--- src/main/java/com/nhnacademy/marketgg/server/service/order/OrderService.java
+++ src/main/java/com/nhnacademy/marketgg/server/service/order/OrderService.java
@@ -96,12 +96,4 @@ public interface OrderService {
         return Long.valueOf(orderId.substring(orderId.indexOf("_") + 1));
     }
 
-    default String attachPrefix(final Long orderId) {
-        return "GGORDER_" + orderId;
-    }
-
-    default Long detachPrefix(final String orderId) {
-        return Long.valueOf(orderId.substring(orderId.indexOf("_") + 1));
-    }
-
 }
