e8fecf9f4d57669bf3e99b64032173300b9367bf
cororing <corock.dev@gmail.com>
2022-08-21 14:48:28 +0900

    Fix: 사용하지 않는 오버라이딩 메서드 삭제 #239

--- src/main/java/com/nhnacademy/marketgg/server/service/payment/MobilePhonePaymentService.java
+++ src/main/java/com/nhnacademy/marketgg/server/service/payment/MobilePhonePaymentService.java
@@ -1,8 +1,5 @@
 package com.nhnacademy.marketgg.server.service.payment;
 
-import com.nhnacademy.marketgg.server.dto.payment.result.MobilePhonePaymentResult;
-import com.nhnacademy.marketgg.server.entity.payment.MobilePhonePayment;
-
 /**
  * 휴대폰 결제에 대한 비즈니스 처리를 담당합니다.
  *
@@ -11,10 +8,4 @@ import com.nhnacademy.marketgg.server.entity.payment.MobilePhonePayment;
  * @since 1.0
  */
 public interface MobilePhonePaymentService extends PaymentService {
-
-    @Override
-    default MobilePhonePayment toEntity(MobilePhonePaymentResult mobilePhoneResult) {
-        return PaymentService.super.toEntity(mobilePhoneResult);
-    }
-
 }
