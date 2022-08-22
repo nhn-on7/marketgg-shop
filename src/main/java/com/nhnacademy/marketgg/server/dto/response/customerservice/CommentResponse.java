872f92114fb55334179b69d6175b13951491c043
psw4887 <83569822+psw4887@users.noreply.github.com>
2022-08-22 11:01:37 +0900

    Feat: Response Swagger 적용 완료 #252

--- src/main/java/com/nhnacademy/marketgg/server/dto/response/customerservice/CommentResponse.java
+++ src/main/java/com/nhnacademy/marketgg/server/dto/response/customerservice/CommentResponse.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.marketgg.server.dto.response.customerservice;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -8,10 +9,13 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public class CommentResponse {
 
+    @Schema(name = "댓글 내용", description = "1:1 문의 댓글의 내용입니다.", example = "싫다니깐요")
     private final String content;
 
+    @Schema(name = "작성자", description = "1:1 문의 댓글의 작성자입니다.", example = "윤동열")
     private final String name;
 
+    @Schema(name = "작성일자", description = "1:1 문의 댓글의 작성일자입니다.", example = "2020-07-10T15:00:00.000")
     private final LocalDateTime createdAt;
 
 }
--- src/main/java/com/nhnacademy/marketgg/server/dto/response/point/PointRetrieveResponse.java
+++ src/main/java/com/nhnacademy/marketgg/server/dto/response/point/PointRetrieveResponse.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.marketgg.server.dto.response.point;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -14,16 +15,22 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public class PointRetrieveResponse {
 
+    @Schema(name = "회원 번호", description = "회원의 식별번호입니다.", example = "1")
     private final Long memberId;
 
+    @Schema(name = "주문 번호", description = "주문의 식별번호입니다.", example = "1")
     private final Long orderNo;
 
+    @Schema(name = "포인트", description = "포인트의 값입니다.", example = "1000")
     private final Integer point;
 
+    @Schema(name = "누적포인트", description = "누적된 포인트의 값입니다.", example = "3000")
     private final Integer totalPoint;
 
+    @Schema(name = "적립/사용 내용", description = "포인트 적립/사용의 내용입니다.", example = "구매")
     private final String content;
 
+    @Schema(name = "내역일시", description = "적립/사용된 일시입니다.", example = "2020-07-10T15:00:00.000")
     private final LocalDateTime updatedAt;
 
 }
