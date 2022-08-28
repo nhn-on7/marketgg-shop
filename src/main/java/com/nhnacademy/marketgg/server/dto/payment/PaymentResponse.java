0bce6d2cab25cc9c3e9ec30fc0b69bfe8d9c08d3
cororing <corock.dev@gmail.com>
2022-08-28 22:39:46 +0900

    Docs: 결제 응답 객체 문서화 #272

--- src/main/java/com/nhnacademy/marketgg/server/dto/payment/PaymentResponse.java
+++ src/main/java/com/nhnacademy/marketgg/server/dto/payment/PaymentResponse.java
@@ -12,6 +12,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
 
 /**
  * 결제 요청에 대해 결제대행사로부터 전달받는 응답 데이터 클래스입니다.
@@ -40,20 +41,26 @@ public class PaymentResponse {
     @Size(max = 100)
     private String orderName;
 
+    @Schema(title = "결제 처리 상태", description = "결제 처리 상태값을 가질 수 있습니다.", example = "DONE")
     @NotBlank
     @Size(max = 20)
     private String status;
 
+    @Schema(title = "거래키", description = "결제 한 건에 대한 승인 및 취소 거래를 구분하는데 사용합니다.",
+            example = "0A25AEB83EB828C7CBDB3E0C97834557")
     @NotBlank
     @Size(max = 64)
     private String transactionKey;
 
+    @Schema(title = "결제 요청일자", description = "결제 요청이 일어난 날짜와 시간 정보입니다.", example = "2022-01-01T00:00:00+09:00")
     @NotNull
     private String requestedAt;
 
+    @Schema(title = "결제 승인일자", description = "결제 승인이 일어난 날짜와 시간 정보입니다.", example = "2022-01-01T00:00:00+09:00")
     @NotNull
     private String approvedAt;
 
+    @Schema(title = "카드", description = "카드로 결제하면 제공되는 카드 관련 정보입니다..")
     private CardPaymentResult card;
 
     private VirtualAccountPaymentResult virtualAccount;
@@ -82,8 +89,6 @@ public class PaymentResponse {
 
     private String failure;
 
-    private boolean isPartialCancelable;
-
     @NotNull
     private Receipt receipt;
 
@@ -91,9 +96,11 @@ public class PaymentResponse {
     @Size(min = 1, max = 3)
     private String currency;
 
+    @Schema(title = "최종 결제 금액", description = "총 결제 금액입니다.", example = "75600")
     @NotNull
     private Long totalAmount;
 
+    @Schema(title = "잔액", description = "취소할 수 있는 금액(잔고)입니다.", example = "100")
     @NotNull
     private Long balanceAmount;
 
@@ -106,6 +113,7 @@ public class PaymentResponse {
     @NotNull
     private Long taxFreeAmount;
 
+    @Schema(title = "결제 수단", description = "결제할 때 사용한 결제 수단입니다.", example = "카드")
     @NotBlank
     @Size(min = 6, max = 21)
     private String method;
