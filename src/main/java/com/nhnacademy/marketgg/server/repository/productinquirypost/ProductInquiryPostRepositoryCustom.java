fcdac66c4b7e8a85279e3b77210459cf5adbd546
coalong <minay0904@gmail.com>
2022-08-30 01:38:01 +0900

    Feat: 관리자 상품 문의 전체 조회 api 추가

--- src/main/java/com/nhnacademy/marketgg/server/repository/productinquirypost/ProductInquiryPostRepositoryCustom.java
+++ src/main/java/com/nhnacademy/marketgg/server/repository/productinquirypost/ProductInquiryPostRepositoryCustom.java
@@ -29,11 +29,22 @@ public interface ProductInquiryPostRepositoryCustom {
     /**
      * 회원 번호로 전체 상품 문의를 조회합니다.
      *
-     * @param id       - 조회할 회원의 id 입니다.
-     * @param pageable - 요청하는 page 의 정보를 가지고 있습니다.
-     * @return - 상품 문의 List 가 반환됩니다.
+     * @param id        조회할 회원의 id 입니다.
+     * @param pageable  요청하는 page 의 정보를 가지고 있습니다.
+     * @return  상품 문의 List 가 반환됩니다.
      * @author 민아영
      * @since 1.0.0
      */
     Page<ProductInquiryResponse> findAllByMemberNo(final Long id, Pageable pageable);
+
+    /**
+     * 전체 상품 문의를 날짜의 내림차순 기준으로 정렬하여 조회합니다.
+     *
+     * @param pageable 요청하는 page 의 정보를 가지고 있습니다.
+     * @return 상품 문의 List 가 반환됩니다.
+     * @author 민아영
+     * @since 1.0.0
+     */
+    Page<ProductInquiryResponse> findAllByAdmin(final Pageable pageable);
+
 }
