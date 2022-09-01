f37a4da5c2a86fbf4e7caf2da2295903750b14d7
Dongyeol <eastheat10@gmail.com>
2022-09-01 22:01:09 +0900

    Feat: Gateway 에서의 호출만 응답하도록 설정

---
+++ src/main/java/com/nhnacademy/marketgg/server/filter/SecurityFilter.java
@@ -0,0 +1,26 @@
+package com.nhnacademy.marketgg.server.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SecurityFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+        throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        String origin = request.getRemoteAddr();
+        log.warn("Origin: {}", origin);
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+}
