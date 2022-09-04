6337788480da66dcd3eae685d4dafa211fa98985
com-sun <gnlslfflffl@gmail.com>
2022-09-04 17:11:49 +0900

    Test: validation 테스트코드 추가

--- src/test/java/com/nhnacademy/marketgg/server/controller/admin/AdminProductControllerTest.java
+++ src/test/java/com/nhnacademy/marketgg/server/controller/admin/AdminProductControllerTest.java
@@ -6,7 +6,6 @@ import static org.mockito.BDDMockito.then;
 import static org.mockito.BDDMockito.willDoNothing;
 import static org.mockito.Mockito.times;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -31,6 +30,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMultipartHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -93,6 +93,36 @@ class AdminProductControllerTest {
         then(productService).should(times(1))
                             .createProduct(any(ProductCreateRequest.class), any(MockMultipartFile.class));
     }
+
+    @Test
+    @DisplayName("상품 등록시 재고를 65535개 이상으로 등록할경우 테스트")
+    void testCreateProductExceedsTotalStockValidation() throws Exception {
+        ProductCreateRequest wrongProductRequest = Dummy.getDummyProductCreateRequest();
+        ReflectionTestUtils.setField(wrongProductRequest, "totalStock", 65536L);
+        String content = objectMapper.writeValueAsString(wrongProductRequest);
+
+        URL url = getClass().getClassLoader().getResource("img/lee.png");
+        String filePath = Objects.requireNonNull(url).getPath();
+
+        MockMultipartFile file =
+            new MockMultipartFile("image", "img/lee.png", "image/png",
+                                  new FileInputStream(filePath));
+
+        MockMultipartFile dto =
+            new MockMultipartFile("productRequest", "jsondata", "application/json",
+                                  content.getBytes(StandardCharsets.UTF_8));
+
+        this.mockMvc.perform(multipart(DEFAULT_PRODUCT).file(dto)
+                                                       .file(file)
+                                                       .headers(httpHeaders)
+                                                       .contentType(
+                                                           MediaType.APPLICATION_JSON_VALUE)
+                                                       .contentType(
+                                                           MediaType.MULTIPART_FORM_DATA_VALUE)
+                                                       .characterEncoding(StandardCharsets.UTF_8))
+                    .andExpect(status().isBadRequest());
+    }
+
 
     @Test
     @DisplayName("상품 정보 수정하는 테스트")
