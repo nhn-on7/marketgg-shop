46015f420d022b72e1520c51a8b1cd3d03c03f6d
cororing <corock.dev@gmail.com>
2022-09-05 13:45:58 +0900

    Feat: 성공적으로 주문 및 결제 시 사용 쿠폰 및 적립금 사용 처리

--- src/main/java/com/nhnacademy/marketgg/server/service/order/DefaultOrderService.java
+++ src/main/java/com/nhnacademy/marketgg/server/service/order/DefaultOrderService.java
@@ -1,8 +1,10 @@
 package com.nhnacademy.marketgg.server.service.order;
 
+import static com.nhnacademy.marketgg.server.repository.auth.AuthAdapter.checkResult;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.nhnacademy.marketgg.server.constant.payment.PaymentType;
 import com.nhnacademy.marketgg.server.constant.OrderStatus;
+import com.nhnacademy.marketgg.server.constant.payment.PaymentType;
 import com.nhnacademy.marketgg.server.dto.info.AuthInfo;
 import com.nhnacademy.marketgg.server.dto.info.MemberInfo;
 import com.nhnacademy.marketgg.server.dto.info.MemberInfoRequest;
@@ -11,6 +13,7 @@ import com.nhnacademy.marketgg.server.dto.request.order.OrderCreateRequest;
 import com.nhnacademy.marketgg.server.dto.request.order.OrderInfoRequestDto;
 import com.nhnacademy.marketgg.server.dto.request.order.OrderUpdateStatusRequest;
 import com.nhnacademy.marketgg.server.dto.request.order.ProductToOrder;
+import com.nhnacademy.marketgg.server.dto.request.point.PointHistoryRequest;
 import com.nhnacademy.marketgg.server.dto.response.coupon.UsedCouponResponse;
 import com.nhnacademy.marketgg.server.dto.response.deliveryaddress.DeliveryAddressResponse;
 import com.nhnacademy.marketgg.server.dto.response.order.OrderDetailRetrieveResponse;
@@ -18,12 +21,14 @@ import com.nhnacademy.marketgg.server.dto.response.order.OrderFormResponse;
 import com.nhnacademy.marketgg.server.dto.response.order.OrderGivenCoupon;
 import com.nhnacademy.marketgg.server.dto.response.order.OrderRetrieveResponse;
 import com.nhnacademy.marketgg.server.dto.response.order.OrderToPayment;
-import com.nhnacademy.marketgg.server.entity.Coupon;
 import com.nhnacademy.marketgg.server.entity.DeliveryAddress;
+import com.nhnacademy.marketgg.server.entity.GivenCoupon;
 import com.nhnacademy.marketgg.server.entity.Member;
 import com.nhnacademy.marketgg.server.entity.Order;
 import com.nhnacademy.marketgg.server.entity.OrderProduct;
+import com.nhnacademy.marketgg.server.entity.PointHistory;
 import com.nhnacademy.marketgg.server.entity.Product;
+import com.nhnacademy.marketgg.server.entity.UsedCoupon;
 import com.nhnacademy.marketgg.server.eventlistener.event.order.OrderCouponCanceledEvent;
 import com.nhnacademy.marketgg.server.eventlistener.event.order.OrderPointCanceledEvent;
 import com.nhnacademy.marketgg.server.exception.coupon.CouponIsAlreadyUsedException;
@@ -48,23 +53,22 @@ import com.nhnacademy.marketgg.server.repository.pointhistory.PointHistoryReposi
 import com.nhnacademy.marketgg.server.repository.product.ProductRepository;
 import com.nhnacademy.marketgg.server.repository.usedcoupon.UsedCouponRepository;
 import com.nhnacademy.marketgg.server.service.cart.CartProductService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.beans.factory.annotation.Value;
-
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import static com.nhnacademy.marketgg.server.repository.auth.AuthAdapter.checkResult;
-
+/**
+ * 기본적인 주문 서비스 기능을 수행합니다.
+ */
 @Service
 @RequiredArgsConstructor
 public class DefaultOrderService implements OrderService {
@@ -97,32 +101,58 @@ public class DefaultOrderService implements OrderService {
     @Transactional
     @Override
     public OrderToPayment createOrder(final OrderCreateRequest orderRequest, final MemberInfo memberInfo)
-            throws JsonProcessingException {
-        int i = 0;
-        // memo: 주문하는 회원
-        Member member = memberRepository.findById(memberInfo.getId()).orElseThrow(MemberNotFoundException::new);
-        // memo: 주문하는 회원 정보 auth server 조회
-        MemberInfoResponse memberResponse = checkResult(
-                authRepository.getMemberInfo(new MemberInfoRequest(member.getUuid())));
-        // memo: 회원이 선택한 배송지
+        throws JsonProcessingException {
+
+        // MEMO: 주문하는 회원 찾기
+        Member member = memberRepository.findById(memberInfo.getId())
+                                        .orElseThrow(MemberNotFoundException::new);
+
+        // MEMO: 주문하는 회원 정보 auth server 조회
+        MemberInfoResponse memberResponse
+            = checkResult(authRepository.getMemberInfo(new MemberInfoRequest(member.getUuid())));
+
+        // MEMO: 주문한 회원이 선택한 배송지 조회
         DeliveryAddress deliveryAddress = deliveryAddressRepository.findById(orderRequest.getDeliveryAddressId())
                                                                    .orElseThrow(DeliveryAddressNotFoundException::new);
-        // memo: 주문서 폼에서 받은 주문할 상품 ID 목록
+        // MEMO: 주문서 폼에서 받은 주문할 상품 ID 목록
         List<Long> productIds = orderRequest.getProductIds();
-        // memo: 위 ID로 장바구니에 담긴 상품 확인 및 정보 조회 (id, name, price, amount)
-        List<ProductToOrder> cartProducts =
-                cartProductRepository.findCartProductsByProductIds(memberInfo.getCart().getId(), productIds);
-        // memo: 상품 수량만 뽑아내기 (재고 차감을 위해)
-        List<Integer> productAmounts = cartProducts.stream().map(ProductToOrder::getAmount)
-                                                   .collect(Collectors.toList());
+
+        // MEMO: 받아온 상품 ID로 장바구니에 담긴 상품 확인 및 정보 조회 (id, name, price, amount)
+        List<ProductToOrder> cartProducts = cartProductRepository.findCartProductsByProductIds(memberInfo.getCart()
+                                                                                                         .getId(),
+                                                                                               productIds);
+
         List<Product> products = productRepository.findByIds(productIds);
 
+        // MEMO: 주문 엔티티 생성 및 검증 후 데이터 저장
         Order order = new Order(member, deliveryAddress, orderRequest, products.get(0).getName(), products.size());
+        Long memberId = member.getId();
+        GivenCoupon givenCoupon = this.checkOrderValid(orderRequest, memberResponse, memberId);
+        orderRepository.saveAndFlush(order);
 
-        checkOrderValid(orderRequest, memberResponse, member.getId());
-        orderRepository.save(order);
+        // MEMO: 사용 쿠폰 적용
+        UsedCoupon usedCoupon = UsedCoupon.builder()
+                                          .pk(new UsedCoupon.Pk(order.getId(),
+                                                                givenCoupon.getPk().getCouponId(),
+                                                                memberId))
+                                          .order(order)
+                                          .givenCoupon(givenCoupon)
+                                          .build();
 
-        // memo: 상품 재고량 수정, 주문상품 테이블 컬럼 추가
+        usedCouponRepository.save(usedCoupon);
+
+        int usedPoint = -orderRequest.getUsedPoint();
+        pointRepository.save(new PointHistory(member, order,
+                                              pointRepository.findLastTotalPoints(memberId) + usedPoint,
+                                              new PointHistoryRequest(usedPoint, "포인트 사용")));
+
+        // MEMO: 재고 차감을 위해 상품 수량만 뽑아내기
+        List<Integer> productAmounts = cartProducts.stream()
+                                                   .map(ProductToOrder::getAmount)
+                                                   .collect(Collectors.toList());
+
+        // MEMO: 상품 재고량 수정, 주문상품 테이블 컬럼 추가
+        int i = 0;
         for (Product product : products) {
             long remain = product.getTotalStock() - productAmounts.get(i);
             if (remain < 0) {
@@ -132,7 +162,8 @@ public class DefaultOrderService implements OrderService {
             productRepository.save(product);
             orderProductRepository.save(new OrderProduct(order, product, productAmounts.get(i++)));
         }
-        // memo: 장바구니 목록 주문한 상품 삭제
+
+        // MEMO: 장바구니 목록 주문한 상품 삭제
         cartProductService.deleteProducts(memberInfo, productIds);
 
         return makeOrderToPayment(order, orderRequest);
@@ -142,33 +173,47 @@ public class DefaultOrderService implements OrderService {
         String orderId = attachPrefix(order.getId());
 
         return new OrderToPayment(orderId, order.getOrderName(), orderRequest.getName(), orderRequest.getEmail(),
-                orderRequest.getTotalAmount(), orderRequest.getCouponId(),
-                orderRequest.getUsedPoint(), orderRequest.getExpectedSavePoint());
+                                  orderRequest.getTotalAmount(), orderRequest.getCouponId(),
+                                  orderRequest.getUsedPoint(), orderRequest.getExpectedSavePoint());
     }
 
-    private void checkOrderValid(final OrderCreateRequest orderRequest, final MemberInfoResponse memberResponse,
-                                 final Long memberId) {
+    /**
+     * 쿠폰 및 포인트 내역이 최총 결제 금액 적용에 유효한지 체크합니다.
+     *
+     * @param orderRequest   주문 요청 객체
+     * @param memberResponse 주문한 회원 정보
+     * @param memberId       주문한 회원 아이디
+     */
+    private GivenCoupon checkOrderValid(final OrderCreateRequest orderRequest, final MemberInfoResponse memberResponse,
+                                        final Long memberId) {
         if (!memberResponse.getEmail().equals(orderRequest.getEmail())) {
             throw new OrderMemberNotMatchedException();
         }
-        Optional<Coupon> coupon = checkCouponValid(orderRequest.getCouponId());
-        if (coupon.isPresent() && coupon.get().getMinimumMoney() > orderRequest.getTotalOrigin()) {
+        Optional<GivenCoupon> givenCoupon = this.checkCouponValid(orderRequest.getCouponId(), memberId);
+        if (givenCoupon.isPresent() && (givenCoupon.get()
+                                                   .getCoupon()
+                                                   .getMinimumMoney() > orderRequest.getTotalOrigin())) {
             throw new CouponNotOverMinimumMoneyException();
         }
         if (pointRepository.findLastTotalPoints(memberId) < orderRequest.getUsedPoint()) {
             throw new PointNotEnoughException();
         }
+
+        return givenCoupon.get();
     }
 
-    private Optional<Coupon> checkCouponValid(final Long couponId) {
+    private Optional<GivenCoupon> checkCouponValid(final Long couponId, final Long memberId) {
         if (Objects.isNull(couponId)) {
             return Optional.empty();
         }
-        Coupon coupon = couponRepository.findById(couponId).orElseThrow(CouponNotFoundException::new);
+        GivenCoupon givenCoupon = givenCouponRepository.findById(new GivenCoupon.Pk(couponId, memberId))
+                                                       .orElseThrow(CouponNotFoundException::new);
+
         if (usedCouponRepository.existsCouponId(couponId)) {
             throw new CouponIsAlreadyUsedException();
         }
-        return Optional.of(coupon);
+
+        return Optional.of(givenCoupon);
     }
 
     /**
@@ -187,12 +232,12 @@ public class DefaultOrderService implements OrderService {
         List<OrderGivenCoupon> orderGivenCoupons = givenCouponRepository.findOwnCouponsByMemberId(memberId);
         Integer totalPoint = pointRepository.findLastTotalPoints(memberId);
         List<DeliveryAddressResponse> deliveryAddresses = deliveryAddressRepository.findDeliveryAddressesByMemberId(
-                memberId);
+            memberId);
         List<String> paymentTypes = Arrays.stream(PaymentType.values())
                                           .map(PaymentType::getType)
                                           .collect(Collectors.toList());
         List<ProductToOrder> cartProducts =
-                cartProductRepository.findCartProductsByProductIds(memberInfo.getCart().getId(), productIds);
+            cartProductRepository.findCartProductsByProductIds(memberInfo.getCart().getId(), productIds);
 
         return OrderFormResponse.builder()
                                 .products(cartProducts)
@@ -237,7 +282,7 @@ public class DefaultOrderService implements OrderService {
     @Override
     public OrderDetailRetrieveResponse retrieveOrderDetail(final Long orderId, final MemberInfo memberInfo) {
         OrderDetailRetrieveResponse detailResponse = orderRepository.findOrderDetail(orderId, memberInfo.getId(),
-                memberInfo.isAdmin());
+                                                                                     memberInfo.isAdmin());
         List<ProductToOrder> products = orderProductRepository.findByOrderId(orderId);
         UsedCouponResponse couponName = usedCouponRepository.findUsedCouponName(orderId);
         detailResponse.addOrderDetail(products, couponName);
@@ -295,10 +340,10 @@ public class DefaultOrderService implements OrderService {
         MemberInfoResponse memberResponse = checkResult(authRepository.getMemberInfo(new MemberInfoRequest(uuid)));
 
         OrderInfoRequestDto orderRequest = new OrderInfoRequestDto(memberResponse.getName(), order.getAddress(),
-                order.getDetailAddress(),
-                memberResponse.getPhoneNumber(),
-                String.valueOf(orderId),
-                successHost);
+                                                                   order.getDetailAddress(),
+                                                                   memberResponse.getPhoneNumber(),
+                                                                   String.valueOf(orderId),
+                                                                   successHost);
 
         deliveryRepository.createTrackingNo(orderRequest);
     }
